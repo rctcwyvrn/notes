60880769387e30b7d3005166d9da74f3b23b511a
Lily Lin <arcticfox517@gmail.com>
2020-08-12 20:33:36 -0700

    Update notes

--- dotfiles/.vimrc
+++ dotfiles/.vimrc
@@ -51,5 +51,6 @@ setlocal spell
 set spelllang=en_us
 inoremap <C-l> <c-g>u<Esc>[s1z=`]a<c-g>u
 
+set rtp+=~/notes/current-course
 
 set visualbell
--- push.sh
+++ push.sh
@@ -1,4 +1,11 @@
+echo "Compiling pdfs"
 python3 compile_latex.py
+
+echo "Copying dotfiles"
+cp ~/.vimrc* dotfiles
+cp ~/.vim/UltiSnips/tex.snippets dotfiles
+cp ~/.vim/spell/en.utf-8.add dotfiles
+
 git add -A
 git commit -m "Update notes"
 git push
